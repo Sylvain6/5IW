11efa5a4ace0e9abc06f58f3c2334d1511992cf7
Adrien Morin <amorin@slyvent.com>
2018-10-27 15:47:43 +0200

    Init projet on Github

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use App\Entity\Tag;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ArticleFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = \Faker\Factory::create();
+
+        $tags = $manager->getRepository(Tag::class)->findAll();
+
+        for ($i = 0; $i < 20; $i++) {
+            $article = (new Article())
+                ->setTitle($faker->firstName)
+                ->setContent($faker->text())
+                ->addTag($tags[array_rand($tags)])
+            ;
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            TagFixtures::class
+        ];
+    }
+}
